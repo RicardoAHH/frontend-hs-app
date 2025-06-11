a45e2bc63157345196df429a118bbd0ca363a5a4
ElderSud <eldercuases@gmail.com>
2025-06-11 09:34:24 -0500

    initial commit

---
+++ src/components/ButtonLink.jsx
@@ -0,0 +1,37 @@
+import React from 'react';
+
+
+export default function ButtonLink({
+  role,
+  onUpdateProfile,
+  onListUsers,
+  onListRoles,
+  onListServices
+}) {
+  const isAdmin = role === 'Admin';
+
+  const btnClass =
+    'w-full text-center bg-[#2c7ee2] hover:bg-[#5991d5] text-white font-semibold py-2 px-4 rounded-lg mb-2 transition-all duration-300 ease-in-out focus:outline-none focus:ring-2 focus:ring-[#2c7ee2] focus:ring-opacity-75';
+
+  return (
+    <div className="space-y-2 p-4 ">
+      <button onClick={onUpdateProfile} className={btnClass}>
+        Actualizar perfil
+      </button>
+
+      {isAdmin && (
+        <button onClick={onListUsers} className={btnClass}>
+          Listar usuarios
+        </button>
+      )}
+
+      <button onClick={onListRoles} className={btnClass}>
+        Listar roles
+      </button>
+
+      <button onClick={onListServices} className={btnClass}>
+        Listar servicios
+      </button>
+    </div>
+  );
+}
---
+++ src/components/ProfileInfo.jsx
@@ -0,0 +1,55 @@
+import React from 'react';
+
+export default function ProfileInfo({ profile }) {
+  if (!profile) return null; // No renderizar nada si aún no hay datos
+
+ const { full_name, email, student } = profile;
+
+   const country    = student?.country    ?? null;
+  const controller = student?.controller ?? null;
+  const recruiter  = student?.recruiter  ?? null;
+
+  return (
+    <div className="bg-white shadow-lg rounded-lg p-6 max-w-md mx-auto flex flex-col items-center">
+      {/* Imagen de perfil placeholder */}
+      <img
+        src="https://www.estudiantefunval.org/theme/image.php/moove/core/1729720886/u/f1"
+        alt="Avatar placeholder"
+        className="w-32 h-32 rounded-full mb-4"
+      />
+
+      {/* Nombre completo */}
+      <h2 className="text-2xl font-semibold text-gray-800 mb-2">
+        {full_name}
+      </h2>
+
+      {/* Email */}
+      <p className="text-gray-600 mb-4">
+        {email}
+      </p>
+
+      {/* Solo si es estudiante: mostrar país, controlador y reclutador */}
+      {student && (
+        <div className="w-full space-y-2 text-gray-700">
+          {student.country && (
+            <p>
+              <span className="font-medium">País:</span> {country.name}
+            </p>
+          )}
+
+          {student.controller && (
+            <p>
+              <span className="font-medium">Controlador:</span> {controller.full_name}
+            </p>
+          )}
+
+          {student.recruiter && (
+            <p>
+              <span className="font-medium">Reclutador:</span> {recruiter.full_name}
+            </p>
+          )}
+        </div>
+      )}
+    </div>
+  );
+}
