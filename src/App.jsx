174eb2db07e857293ffe69421aa2b8aa97746b83
RicardoAHH <richhquim@gmail.com>
2025-06-16 13:32:15 -0600

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -40,7 +40,7 @@ function App() {
     <>
       <Routes>
         <Route path="/" element={<Home />} />
-        <Route path="/login" element={<Login />} />
+        <Route path="/" element={<Login />} />
         <Route
           path="/profile"
           element={
